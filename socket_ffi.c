904f80ac31cba9ba51f341281aa33c1a4cf1113a
Grant Jurgensen <gajurgensen@gmail.com>
2019-01-29 14:03:28 -0600

    Handle errors on the CakeML side

--- socket_ffi.c
+++ socket_ffi.c
@@ -3,17 +3,13 @@
 // This socket interface is largely based on the example provided in the
 // getaddrinfo man page http://man7.org/linux/man-pages/man3/getaddrinfo.3.html
 
-#include <assert.h>     // asserts
-#include <stdint.h>     // uint8_t, uint16_6, etc.
+// #include <assert.h>     // asserts
+#include <stdint.h>     // uint8_t
 #include <sys/types.h>  // socket, bind, listen, getaddrinfo
 #include <sys/socket.h> // socket, bind, listen, getaddrinfo
 #include <netdb.h>      // getaddrinfo
-#include <string.h>     // memset, strerror
+#include <string.h>     // memset
 #include <unistd.h>     // close
-#include <stdio.h>      // fprintf
-#include <stdlib.h>     // exit
-#include <errno.h>      // errno
-// #include <fcntl.h>      // fcntl
 
 // These helper functions are defined in basis_ffi.c
 // I figured it would be good to use the same marshalling paradigm as the
@@ -25,22 +21,6 @@ void int_to_byte2(int i, uint8_t *b);
 int byte8_to_int(uint8_t *b);
 void int_to_byte8(int i, uint8_t *b);
 
-// Error helper functions. Similar to asserts, but unconditional and with a msg
-#ifndef NDEBUG
-#define fatalErr(msg) __fatalErr(msg, __FILE__, __LINE__)
-#define nonfatalErr(msg) __nonfatalErr(msg, __FILE__, __LINE__)
-void __fatalErr(const char * msg, const char * file, int line) {
-    fprintf(stderr, "%s:%d: Fatal error: %s\n", file, line, msg);
-    abort();
-}
-void __nonfatalErr(const char * msg, const char * file, int line) {
-    fprintf(stderr, "%s:%d: Nonfatal error: %s\n", file, line, msg);
-}
-#else
-#define fatalErr(msg)
-#define nonfatalErr(msg)
-#endif
-
 ////////////////////////////////////////////////////////////////////////////////
 // Server functions:                                                          //
 ////////////////////////////////////////////////////////////////////////////////
@@ -49,8 +29,8 @@ void __nonfatalErr(const char * msg, const char * file, int line) {
 //     number, following qlen
 // Returns: sockfd as 64-bit int in a
 void ffilisten(uint8_t * c, long clen, uint8_t * a, long alen) {
-    assert(clen >= 2);
-    assert(alen >= 8);
+    // assert(clen >= 2);
+    // assert(alen >= 8);
 
     // Parse arguments
     int qlen = byte2_to_int(c);
@@ -68,7 +48,11 @@ void ffilisten(uint8_t * c, long clen, uint8_t * a, long alen) {
     hints.ai_flags = AI_PASSIVE;
     struct addrinfo * result;
     int gai_ret = getaddrinfo(0, port, &hints, &result);
-    if (gai_ret) fatalErr(gai_strerror(gai_ret));
+    if (gai_ret) {
+        freeaddrinfo(result);
+        a[0] = 1;
+        return;
+    }
 
     int sockfd;
     struct addrinfo * r;
@@ -78,19 +62,18 @@ void ffilisten(uint8_t * c, long clen, uint8_t * a, long alen) {
             continue;
 
         int enable = 1;
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)))
-            nonfatalErr(strerror(errno));
+        setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable));
 
-        if (bind(sockfd, r->ai_addr, r->ai_addrlen)) {
-            nonfatalErr(strerror(errno));
-            close(sockfd);
-        }
-        else
+        if (!bind(sockfd, r->ai_addr, r->ai_addrlen))
             break;
+
+        close(sockfd);
     }
-    assert(r);
-    assert(sockfd != -1);
     freeaddrinfo(result);
+    if (!r || sockfd == -1) {
+        a[0] = 1;
+        return;
+    }
 
     // Listen for incoming connections, with a maximum queue length of qlen
     listen(sockfd, qlen);
@@ -104,8 +87,8 @@ void ffilisten(uint8_t * c, long clen, uint8_t * a, long alen) {
 // Returns: conn_sockfd as 64-bit int in a
 // Blocks until there is an incoming connection
 void ffiaccept(uint8_t * c, long clen, uint8_t * a, long alen) {
-    assert(clen >= 8);
-    assert(alen >= 8);
+    // assert(clen >= 8);
+    // assert(alen >= 8);
 
     // Parse argument
     int sockfd = byte8_to_int(c);
@@ -115,11 +98,10 @@ void ffiaccept(uint8_t * c, long clen, uint8_t * a, long alen) {
     // accept returns the sockfd corresponding to the first connection in the
     // incoming queue. If there is none, blocks until there is.
     int conn_sockfd = accept(sockfd, (struct sockaddr *)(&conn_addr), &conn_addr_len);
-    assert(conn_sockfd != -1);
-
-    // Set nonblocking
-    // int flags = fcntl(conn_sockfd, F_GETFL, 0);
-    // fcntl(conn_sockfd, F_SETFL, flags | O_NONBLOCK);
+    if (conn_sockfd == -1) {
+        a[0] = 1;
+        return;
+    }
 
     // return conn_sockfd
     a[0] = 0;
@@ -135,8 +117,8 @@ void ffiaccept(uint8_t * c, long clen, uint8_t * a, long alen) {
 //     again as a string. port should be followed by a final null byte.
 // Returns: sockfd as 64-bit int in a
 void fficonnect(uint8_t * c, long clen, uint8_t * a, long alen) {
-    assert(clen >= 2); // Assumes there are at least the null byte delimiter and terminator
-    assert(alen >= 8);
+    // assert(clen >= 2); // Assumes there are at least the null byte delimiter and terminator
+    // assert(alen >= 8);
 
     // Parse arguments
     char * host = c;
@@ -151,7 +133,11 @@ void fficonnect(uint8_t * c, long clen, uint8_t * a, long alen) {
     hints.ai_socktype = SOCK_STREAM;
     struct addrinfo * result;
     int gai_ret = getaddrinfo(host, port, &hints, &result);
-    if (gai_ret) fatalErr(gai_strerror(gai_ret));
+    if (gai_ret) {
+        freeaddrinfo(result);
+        a[0] = 1;
+        return;
+    }
 
     int sockfd;
     struct addrinfo * r;
@@ -160,20 +146,16 @@ void fficonnect(uint8_t * c, long clen, uint8_t * a, long alen) {
         if (sockfd == -1)
             continue;
 
-        if (connect(sockfd, r->ai_addr, r->ai_addrlen)) {
-            nonfatalErr(strerror(errno));
-            close(sockfd);
-        }
-        else
+        if (!connect(sockfd, r->ai_addr, r->ai_addrlen))
             break;
-    }
-    assert(r);
-    assert(sockfd != -1);
-    freeaddrinfo(result);
 
-    // Set nonblocking
-    // int flags = fcntl(sockfd, F_GETFL, 0);
-    // fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+        close(sockfd);
+    }
+    freeaddrinfo(result);
+    if (!r || sockfd == -1){
+        a[0] = 1;
+        return;
+    }
 
     // return sockfd
     a[0] = 0;
